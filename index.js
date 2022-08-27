59d5c92df0c2c6de2a70d893fabef6502888cefe
VanessaSayuri <sayurivanessa@gmail.com>
2022-08-27 20:07:02 -0300

    add field validation last name
    still not fixed the error message of the field Last Name

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fields = document.querySelectorAll("[required]")
 
 //console.log(fields);
 
-function customValidation(event){
-    const field = event.target;
 
-    //lógica para verificar se existem erros (propriedade customError == true)
+//função para validar os campos
+function ValidateField(field){
+    //lógica para verificar se existem erros
     function verifyErrors(){
         let foundError = false;
 
 
         for (let error in field.validity){ //in intera sobre propriedades do objeto
             // se nao for customError então verifica se tem erro
-            if(error != "customError" && field.validity[error]){
-                foundError = true
+            if(field.validity[error] && !field.validity.valid){
+                foundError = error
             }
         } 
 
@@ -22,27 +22,56 @@ function customValidation(event){
 
     }
 
-    const error = verifyErrors();
+    function setCustomMessage (message){
+        const spanError = field.parentNode.querySelector("span.error")
+        if(message){
+            //trocar a mensagem de required
+        //field.setCustomValidity("First Name can not be empty!")
 
-    if (error){
-        //trocar a mensagem de required
-        field.setCustomValidity("First Name can not be empty!")
-    }
-    else{
-        field.setCustomValidity("")
+            spanError.classList.add("active"); //adiciona a classe e insere a mensagem
+            spanError.innerHTML = message
+    
+        } else{
+             //field.setCustomValidity("")
+            spanError.classList.remove("active");
+            spanError.innerHTML = ""
+        }
+        
     }
 
+    return function(){
+        if (verifyErrors()){
+            field.style.borderColor = "red"
+            setCustomMessage ("First Name can not be empty!")
+        }
+        else{
+            setCustomMessage()
+        }
+    };
+}
+
+
+function customValidation(event){
+
+    const field = event.target;
+
+    const validation = ValidateField(field);
+
+    validation();
     
 }
 
 
 
 for(let field of fields){
-    field.addEventListener("invalid", customValidation)
-}
-
-
-
+    field.addEventListener("invalid", event => {
+        //elliminar o bubble
+        event.preventDefault() // só faz sentido usar ele aqui pois o bubble só exite no evento "inválido"
+        customValidation (event)
+    })
+   
+    field.addEventListener("blur", customValidation)
+} 
 
 
 
